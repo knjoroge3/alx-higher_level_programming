6c2e6e2ebcc10868433174e57cddf073d4315314
knjoroge3 <njorogeken57@gmail.com>
2025-07-13 07:43:02 +0300

    Game over

---
+++ 0x00-learning/game_over.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# This is a game over ASCII art made by no other than KKN
+# the r before the tripple quotes is for escaping backslash
+# Tripple quotes allow for multi line strings
+
+print(r"""
+      _____      ___       ___  ___   _____
+    / _____|    /   |     /   |/   | | ____|
+    | |        / /\ |    / /|   /| | | |___
+    | |   _   / __  |   / / |__/ | | |  ___|
+    | |_ | | / /  | |  / /       | | | |____
+    \_____/ /_/   |_| /_/        |_| |_____|
+
+
+
+      ____   _       _  ____    ____
+    /  _  \  | |   / / |  __|  |  _  \ 
+    | | |  | | |  / /  | |__   | |_| |
+    | | |  | | | / /   |  __|  |  _  /
+    | | |  | | |/ /    | |__   | |  \ \ 
+    \ ____ / |___/     | ___|  |_|   \_\    
+
+    """)
