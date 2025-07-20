045e4ffe04676fc86a2182b1b64bb77ed8aa5988
knjoroge3 <njorogeken57@gmail.com>
2025-07-20 18:40:29 +0300

    infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,9 +4,9 @@ import sys
 
 # Write a program that prints the result of the addition of all arguments
 
-if __name__=="__main__":
+if __name__ == "__main__":
     args = sys.argv[1:]
     total = 0
     for i in args:
-            total += int(i)
+        total += int(i)
     print("{}".format(total))
