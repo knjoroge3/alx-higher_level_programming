b1ddeeb9025f23d16c78be573d4423ccbf97546e
knjoroge3 <njorogeken57@gmail.com>
2025-07-28 09:45:44 +0300

    More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,9 +2,7 @@
 
 def multiple_returns(sentence):
 
-    first_character = sentence[0]
-
     if len(sentence) == 0:
         return (0, None)
     else:
-        return (len(sentence), first_character)
+        return (len(sentence), sentence[0])
