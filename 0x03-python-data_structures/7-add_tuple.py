342a747a80942e3f0c64edc241f422de8645e02a
knjoroge3 <njorogeken57@gmail.com>
2025-07-28 09:10:49 +0300

    Tuples addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    tuple_a = tuple_a[:2]
+    tuple_b = tuple_b[:2]
+
+    if len(tuple_a) > 0:
+        i = tuple_a[0]
+    else:
+        i = 0
+    if len(tuple_a) > 1:
+        j = tuple_a[1]
+    else:
+        j = 0
+    if len(tuple_b) > 0:
+        k = tuple_b[0]
+    else:
+        k = 0
+    if len(tuple_b) > 1:
+        m = tuple_b[1]
+    else:
+        m = 0
+
+    return (i + k, j + l)
