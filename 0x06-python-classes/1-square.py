ff7e9d58534f1dfe4c0572476803ed8e69dd7426
knjoroge3 <njorogeken57@gmail.com>
2025-08-07 20:57:26 +0300

    Square with Size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This code writes a square an instantiates it"""
+
+
+class Square:
+    """this is the class square """
+    def __init__(self, size):
+        """this creates a method with two arguments including size"""
+        self.__size = size
