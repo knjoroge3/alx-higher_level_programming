047524329dac43245e3d20a52c2c10ada93219bd
knjoroge3 <njorogeken57@gmail.com>
2025-08-08 23:00:07 +0300

    Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+This module has a class square that checks both type and vallue
+error of the oject
+"""
+
+
+class Square:
+    """
+    This is a class Square that checks the size attribute properly
+    """
+    def __init__(self, size=0):
+        """
+        Constructor that initializes size with type and value checks.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
