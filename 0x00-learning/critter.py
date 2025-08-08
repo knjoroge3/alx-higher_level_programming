0f6999c4cab2cb09573b708d0bedce23e81371bf
knjoroge3 <njorogeken57@gmail.com>
2025-08-08 15:47:07 +0300

    Will be back

---
+++ 0x00-learning/critter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""The Critter Caretaker program charges the user with the care of his or her own
+virtual pet.
+The user must feed and play with the critter to
+keep it in a good mood. The user can listen to the critter to learn how the critter
+is feeling, which can range from happy to mad.
+"""
+
+
+class Critter:
+    pass
