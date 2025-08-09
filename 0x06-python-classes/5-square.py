0f6153b8f5b5d50f7b01db9631198d0f11ff2d20
knjoroge3 <njorogeken57@gmail.com>
2025-08-09 07:00:42 +0300

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+"""
+Write a class Square that defines a square.
+"""
+
+
+class Square:
+    """
+    the blue print of the object square
+    """
+    def __init__(self, size=0):
+        """
+        Constructor that initializes size with type and value checks
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        return the size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        set size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return the area of the square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for row in range(self.__size):
+                for row in range(self.__size):
+                    print("#", end='')
+                print()
+
+                """
+                i have seen however that there are other ways of
+                multiplying a string eg.
+
+                for i in range(size):
+                    print("#" * size)
+                    """
