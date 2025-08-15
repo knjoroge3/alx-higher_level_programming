4be750837f34c94d9e733846613c811a83400235
knjoroge3 <njorogeken57@gmail.com>
2025-08-15 15:15:56 +0300

    Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Write an empty class Rectangle that defines a rectangle
+You are not allowed to import any module
+"""
+
+
+class Rectangle:
+    pass
+
+
+rectangle = Rectangle()
