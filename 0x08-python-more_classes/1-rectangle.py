fad4f96f4ea58d804eeca58eba206e73fce5d8c8
knjoroge3 <njorogeken57@gmail.com>
2025-08-18 19:53:09 +0300

    The simplest class ever

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Write an empty class Rectangle that defines a rectangle
+You are not allowed to import any module
+"""
+
+
+class Rectangle:
+    """
+    an instane of the Rectangle Class
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self, value):
+        if width isinstance
+rectangle = Rectangle()
---
+++ 0x08-python-more_classes/learn.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+this orogram attempts to see if I am getting what OOP is all about.
+
+To explain we have classes and objects
+Objects are instances of classes
+
+this means if we have a variable of type int then the variable itself is
+an instance of the int class.
+"""
+
+class Myclass:
+    pass
+
+p = Myclass()
+print(p)
