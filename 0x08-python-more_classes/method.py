cba7ce685283e4396274ed9a65e62076abd547d8
knjoroge3 <njorogeken57@gmail.com>
2025-08-18 20:17:57 +0300

    Creating methods

---
+++ 0x08-python-more_classes/method.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this code shows the how methods are created and what they are
+"""
+
+
+class MyClass:  # creating the class
+    def say_hi(self):  # creating the method.
+        print("Hello Hello People")  # What the methos will do
+
+
+p = MyClass()  # Creating an instance of the class
+p.say_hi()  # Calling the method
