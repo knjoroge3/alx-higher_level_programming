33207348a5cc2bbb468290f50352a7b2ab8bad77
knjoroge3 <njorogeken57@gmail.com>
2025-08-18 20:04:28 +0300

    The simplest class ever

--- 0x08-python-more_classes/learn.py
+++ 0x08-python-more_classes/learn.py
@@ -10,8 +10,23 @@ this means if we have a variable of type int then the variable itself is
 an instance of the int class.
 """
 
-class Myclass:
-    pass
+class Myclass: 
+    """
+    Myclass is the name of the class
+    """
+   pass
+"""
+where this pass is this is where we find the body of the class
+"""
 
 p = Myclass()
+"""
+p here is the object or instance of the class and is created by
+passing the name of the class which is Myclass followed by a pair of 
+parentheses
+"""
 print(p)
+"""
+when you print p you notice that the output is the name of the object
+stored at a particular address
+"""
