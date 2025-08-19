ac9ce7ffc997a07c8115409315f9f47bb147af26
knjoroge3 <njorogeken57@gmail.com>
2025-08-19 21:18:51 +0300

    Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,13 +8,55 @@ You are not allowed to import any module
 
 class Rectangle:
     """
-    an instane of the Rectangle Class
+    an instance of the Rectangle Class
     """
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        """
+        the initialization step
+        """
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self, value):
-        if width isinstance
-rectangle = Rectangle()
+        """
+        gettin width so that I can  proceed to set it
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setting width and checking is it is an int or great than 0
+        """
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            return self.__width
+
+    @property
+    def height(self, value):
+        """
+        Getting height so that I can proceed to set it
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        setting height and checking if it is an int and greater than 0
+        """
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            return self.__height
+
+
+rectangle = Rectangle()  # instantiating the object
