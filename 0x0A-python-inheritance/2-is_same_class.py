f6c08cccb87bbb17f76b2f23c69f403fee766e68
knjoroge3 <njorogeken57@gmail.com>
2025-08-26 19:56:30 +0300

    Exact Same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pyhton3
+
+"""
+this code returns True if the
+if the object is exactly an instance of the
+specified class ; otherwise False
+"""
+
+def is_same_class(obj, a_class):
+    """
+    function Prototype defination
+    """
+    if type(obj) == a_class:
+        return True
