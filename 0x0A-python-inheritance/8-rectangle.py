1159cc406891e715e9cb5167d43bc1708e128ec2
knjoroge3 <njorogeken57@gmail.com>
2025-08-26 21:30:21 +0300

    Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+An Empty class base
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class defination
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        else:
+            return value
+
+
+class Rectangle(BaseGeometry):
+    """
+    a derived class from BaseGeometry
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+
+base = BaseGeometry()
