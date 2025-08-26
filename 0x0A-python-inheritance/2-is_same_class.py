24e3be5f11f5a7558019201c5be0d369342c4b29
knjoroge3 <njorogeken57@gmail.com>
2025-08-26 20:22:31 +0300

    Is instance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,11 +6,12 @@ if the object is exactly an instance of the
 specified class ; otherwise False
 """
 
+
 def is_same_class(obj, a_class):
     """
     function Prototype defination
     """
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     else:
         return False
