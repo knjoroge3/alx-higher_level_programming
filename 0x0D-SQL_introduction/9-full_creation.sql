6462f9cf05fd15b042e47a7aeffca643157d28b7
knjoroge3 <njorogeken57@gmail.com>
2025-08-30 21:59:23 +0300

    Full table"

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,15 +3,14 @@
 -- Write a script that creates a table second_table in the 
 -- database hbtn_0c_0 in your MySQL server and add multiples rows
 
-CREATE IF NOT EXISTS TABLE second_table(
+CREATE TABLE IF NOT EXISTS second_table(
 	id INT,
 	name VARCHAR(256),
 	score INT
 	);
 
-INSERT INTO TABLE second_table(
-	values (1, “John”, 10),
-	(2, “Alex”, 3),
-	(3, “Bob”,  14),
-	(4, “George”, 8)
-);
+INSERT INTO TABLE second_table(id, name, score)
+	values (1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob',  14),
+	(4, 'George', 8);
