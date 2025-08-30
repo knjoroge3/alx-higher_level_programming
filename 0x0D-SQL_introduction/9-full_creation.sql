f7458422caae763f2418a7d7a127e13a8abbeecd
knjoroge3 <njorogeken57@gmail.com>
2025-08-30 22:01:21 +0300

    Full table"

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -9,8 +9,9 @@ CREATE TABLE IF NOT EXISTS second_table(
 	score INT
 	);
 
-INSERT INTO TABLE second_table(id, name, score)
-	values (1, 'John', 10),
+INSERT INTO second_table(id, name, score)
+	values
+	(1, 'John', 10),
 	(2, 'Alex', 3),
 	(3, 'Bob',  14),
 	(4, 'George', 8);
