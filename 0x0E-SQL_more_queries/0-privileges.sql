26629d204c593ab55eb7e00236c1b3089dc32f8f
knjoroge3 <njorogeken57@gmail.com>
2025-09-01 22:12:17 +0300

    Privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -3,5 +3,5 @@
 -- Write a script that lists all privileges of the
 -- MySQL users user_0d_1 and user_0d_2 on your server
 
-SHOW GRANTS FOR 'user_od_1'@'localhost';
-SHOW GRANTS FOR 'user_od_2'@'localhost';
+SHOW IF EXISTS GRANTS FOR 'user_od_1'@'localhost';
+SHOW IF EXISTS GRANTS FOR 'user_od_2'@'localhost';
